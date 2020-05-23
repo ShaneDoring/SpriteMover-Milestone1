2cbd5e877477e154292b358169ea1648d7ee909b
Shane <sdoring1987@gmail.com>
2020-05-22 19:35:24 -0700

    Added Movement Component for Milestone 1

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Transform tf;   //Holds the trasnform component
+
+    Vector3 myVector = new Vector3(0, 0, 0); // Vector3 for starting position
+
+    public float speed = 0.01f; // variable for designers to manipulate in engine
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();  //Get the Transform component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3.right * speed); //Automaticlly moves sprite across the screen to the right at a speed determined by designer
+    }
+}
